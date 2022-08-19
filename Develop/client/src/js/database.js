f721ba1319a42d727966ade86d890de1562d6781
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-18 20:04:02 -0400

    added the getall

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -28,13 +28,23 @@ export const putDb = async (content) => {
   const result = await request;
   console.log('Data saved!', result);
 };
-// activity 23 - const postdb (might need to be put)
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.error('getDb not implemented');
-  
+  console.log('GET from the database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
-// activity 23 - const getone db
 
 initdb();

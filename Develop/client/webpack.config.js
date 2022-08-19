904110e0d7c76051a2b6ffedd2050b7fab5cd316
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-19 14:18:30 -0400

    added all webpack features

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -22,18 +22,50 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({ 
         template: './index.html', 
-        title: 'Text-Editor-Extreme'
+        title: 'Just Another Text Editor', 
       }),
       new InjectManifest({ 
         swSrc: './src/sw.js',
-        swDest: 'service-worker.js',
+        swDest: 'src-sw.js',
       }),
-      // webpack - fingerprints inject name short name descrip background color,,,,, 
+      new WebpackPwaManifest({ 
+        short_name: 'J.A.T.E.',
+        name: 'Just Another Text Editor',
+        description: 'Text editor to the extreme!',
+        background_color: '#333333',
+        theme_color: '#333333',
+        start_url: './',
+        publicPath: './',
+        fingerprints: false,
+        inject: true,
+        display: 'standalone',
+        icons: [
+          {
+            src: path.resolve("./src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', icons),
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
